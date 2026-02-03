a7d5ac52f46b6f954b02c1cd50120d63db95ef11
deadly-06 <abhimanyu666yadav@gmail.com>
2026-02-03 19:54:46 +0530

    add soln for pg427 q2

--- Pg427_q2.py
+++ Pg427_q2.py
@@ -1,1 +1,11 @@
-
+x=int(input("enter x coordinates"))
+y=int(input("enter y corrdinates"))
+if x>0 and y>0:
+    print("x and y lies in 1st quadrant")
+elif x>0 and y<0:
+    print("x and y lies in 4th quadrant")
+elif x<0 and y>0:
+    print("x and y lies in 2nd quadrant")
+else:
+    print("x and y lies in 3rd quadrant")
+    
