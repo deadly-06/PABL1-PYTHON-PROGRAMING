4eecad3ca6cf1da698b04bc6bf0c20ddbc0acef5
deadly-06 <abhimanyu666yadav@gmail.com>
2026-02-18 21:22:25 +0530

    Create jump game 2.py
    add

---
+++ jump game 2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jump(self, nums):
+        jumps = 0
+        current_end = 0
+        farthest = 0
+        
+        for i in range(len(nums) - 1):
+            # Update the farthest we can reach
+            farthest = max(farthest, i + nums[i])
+            
+            # If we reach the end of current range,
+            # we must make a jump
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+        
+        return jumps
